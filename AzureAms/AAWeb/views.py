48fc60049ce428dfb742f475ed0c537674d483e4
Duong Pham Minh Dung <94067220+duongphamminhdung@users.noreply.github.com>
2022-11-22 20:15:06 +0700

    template for articles

---
+++ AzureAms/AAWeb/views.py
@@ -0,0 +1,58 @@
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import loader
+from django.urls import reverse
+from .models import Article
+
+
+def index(request):
+    myarticles = Article.objects.all().values()
+    #update ID
+    prev = 0
+    for member in Article.objects.all():
+        if member.id != prev+1:
+            temp = member.id
+            member.id = prev+1
+            Article.objects.get(id=temp).delete()
+        prev += 1
+        member.save()
+    #update ID complete
+    template = loader.get_template('index.html')
+    context = {
+    'myarticles': myarticles,
+    }
+    return HttpResponse(template.render(context, request))
+  
+def add(request):
+    template = loader.get_template('add.html')
+    return HttpResponse(template.render({}, request))
+
+def addrecord(request):
+    x = request.POST['title']
+    y = request.POST['content']
+    member = Article(title=x, content=y)
+    member.save()
+    return HttpResponseRedirect(reverse('index'))
+
+def delete(request, id):
+    member = Article.objects.get(id=id)
+    member.delete()
+    return HttpResponseRedirect(reverse('index'))
+
+def update(request, id):
+    myarticle = Article.objects.get(id=id)
+    template = loader.get_template('update.html')
+    context = {
+    'myarticle': myarticle,
+    }
+    return HttpResponse(template.render(context, request))
+
+def updaterecord(request, id):
+    title = request.POST['title']
+    content = request.POST['content']
+    idToChange = request.POST['id']
+    member = Article.objects.get(id=id)
+    member.title = title
+    member.contentname = content
+    member.id = idToChange
+    member.save()
+    return HttpResponseRedirect(reverse('index'))
---
+++ AzureAms/AzureAms/urls.py
@@ -0,0 +1,30 @@
+"""tutorial URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from AAWeb import views
+
+urlpatterns = [
+    path('AzureAms/', include('AAWeb.urls')),
+    path('', views.index, name='index'),
+    path('admin/', admin.site.urls),
+    path('add/', views.add, name='add'),
+    path('add/addrecord/', views.addrecord, name='addrecord'),
+    path('delete/<int:id>', views.delete, name='delete'),
+    path('update/<int:id>', views.update, name='update'),
+    path('update/updaterecord/<int:id>', views.updaterecord, name='updaterecord'),
+
+]
